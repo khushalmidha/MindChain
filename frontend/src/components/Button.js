4cba093ebfcbfc70841d1e84f95a05fcb08c4b9a
khushalmidha <khushalmidha24@gmail.com>
2024-10-20 10:52:25 +0530

    button coloring

--- frontend/src/components/Button.js
+++ frontend/src/components/Button.js
@@ -5,7 +5,7 @@ const Button = ({href, text, color, hiddenOnSmallScreen, pX, pY, mX, mY}) => {
     return (
         <a href={href}>
             <button type="button" 
-                    className={`transition duration-500 ease-in-out ${ hiddenOnSmallScreen ? 'hidden xl:block' : ''} bg-${color}-500 hover:bg-${color}-600 px-${pX} py-${pY} mx-${mX} my-${mY} rounded-full text-white font-bold tracking-normal transform hover:-translate-y-1 ` }>
+                    className={`transition duration-500 ease-in-out ${ hiddenOnSmallScreen ? 'hidden xl:block' : ''} bg-${color}-500 hover:bg-${color}-600 px-${pX} py-${pY} mx-${mX} my-${mY} rounded-full text-black dark:text-white font-bold tracking-normal transform hover:-translate-y-1 ` }>
                 {text}
             </button>
         </a>
